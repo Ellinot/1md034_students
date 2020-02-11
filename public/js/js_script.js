e9d3668a883e76872eb6f7340f0ad966d921d749
Ellinot <ellinor.hallmen@gmail.com>
2020-02-11 14:25:09 +0100

    4-8 Javascript

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,53 +1,77 @@
-function menuItem(name, info, allergy){
+function menuItem(name, img, ingredients, gluten, lactose, kcal){
 this.name = name; // The this keyword refers to the object itself
-this.allergy = allergy;
-this.info = info;
+this.img = img;
+this.ingredients = ingredients;
+this.gluten= gluten;
+this.lactose = lactose;
+this.kcal = kcal;
+
 this.information = function() {
-    return this.info + ' ' + this.allergy;
+
+    return this.ingredients + this.kcal;
 };
 }
 
-let burger1 = new menuItem('Original burger', 'Cheddar cheese \nRoman lettuce, tomatoe and onion \nSourdough bread', 'false', 'true');
+let burger1 = new menuItem('Original burger', './img/burgler1.png', 'Cheddar cheese \nRoman lettuce, onion \n,Sourdough bread', false, true, 'kcal: 2200');
 
-let burger2 = new menuItem('Vegan burger', 'Vegan cheddar \nLettuce, tomatoe and red cabbage \nRhye bread', 'false', 'false');
+let burger2 = new menuItem('Vegan burger', './img/burgler2.png', 'Vegan cheddar \nLettuce, red cabbage \n,Rhye bread', false, false, 'kCal: 790');
 
-let burger3 = new menuItem('Mushroom burger', 'Cheddar cheese, Portabello, spinach & grilled tomatoe \nWheat bread', 'true', 'true');
+let burger3 = new menuItem('Mushroom burger', './img/burgler3.png', 'Cheddar cheese, Portabello, grilled tomatoe \n,Wheat bread', true, true, 'kCal: 1015');
 
-let burger4 = new menuItem('Cheese burger', 'Mozarella, Pickled onion, cucumber \nWheat bread', 'true', 'false');
+let burger4 = new menuItem('Cheese burger', './img/burgler4.png', 'Mozarella, Pickled onion, cucumber \n,Wheat bread', true, false, 'kCal: 1700');
 
-let burger5 = new menuItem('Spicy burger', 'jalapeños, chili, garlic \nSourdough bread', 'false', 'false');
+let burger5 = new menuItem('Spicy burger', './img/burgler6.png', 'jalapeños, chili, garlic, tortilla \n,Sourdough bread', false, true, 'kCal: 2400');
 
+let burger6 = new menuItem('Green burger', './img/burgler5.png', 'Cheddar cheese, vegetables, \n, Wheat bread', false, false, 'kCal: 1200');
 
-let img1 = document.getElementById('./img/burgler1.png');
-
-
-
-var menu = [burger1, burger2, burger3, burger4, burger5];
+var menu = [burger1, burger2, burger3, burger4, burger5, burger6];
 
 var id = document.getElementById("menu");
 
 for (var burger of menu){
-let listItem = document.createElement("li");
+let burgerInfo = document.createElement("div");
+let box = document.createElement("div");
 
-if(burger.gluten == 'true'){
-listItem.innerHTML = burger.information() + ' ' + "contains gluten";
-}
-else if (burger.gluten == 'false'){
-listItem.innerHTML = burger.information();
-}
-else if (burger.lactose == 'true') {
-listItem.innerHTML = burger.information() + ' ' + "contains lactose";
-}
-else {
-listItem.innerHTML = burger.information();
-}
-id.appendChild(listItem);
-}
+let header = document.createElement("h4");
+header.innerHTML = burger.name;
+header.style.marginBottom = "0.5em";
+
+let img = document.createElement("IMG");
+img.src = burger.img;
+img.height = "260";
+
+let list = document.createElement("ul");
+let listingredients = document.createElement("li");
+let listkcal = document.createElement("li");
+let containsGluten = document.createElement("li");
+let containsLactose = document.createElement("li");
+let containsLactGlut = document.createElement("li");
+let glutenText = document.createTextNode("Contains gluten");
+let lactoseText = document.createTextNode("Contains lactose");
+let lactGlutText = document.createTextNode("Contains gluten & lactose");
+listingredients.innerHTML = burger.ingredients;
+listkcal.innerHTML = burger.kcal;
 
 
-/*
-document.getElementById("myID").innerHTML = "Välj en burgare";
+box.appendChild(header).style.textAlign = "center";
+box.appendChild(img);
+box.appendChild(list);
+list.appendChild(listingredients);
+list.appendChild(listkcal);
 
-var burgerOne = document.createElement('p');
-burgerOne.innerHTML = burger1.information();
-document.getElementById("myID").appendChild(burgerOne); */
+if(burger.gluten && burger.lactose){
+containsLactGlut.appendChild(lactGlutText);
+list.appendChild(containsLactGlut).style.fontStyle = "italic";
+}
+if(burger.gluten && !burger.lactose){
+containsGluten.appendChild(glutenText);
+list.appendChild(containsGluten).style.fontStyle = "italic";
+}
+if (burger.lactose && !burger.gluten) {
+containsLactose.appendChild(lactoseText);
+list.appendChild(containsLactose).style.fontStyle = "italic";
+}
+
+document.getElementById("grid").appendChild(box);
+
+}
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,17 +1,17 @@
-document.getElementById("test").innerHTML = "Burger selection";
-
-/*<div id = "burger">
+/*document.getElementById("test").innerHTML = "Burger selection"; */
+/*
+<div id = "burger">
      <div v-for="burger in burgers">
         {{ burger.information() }}
         <span v-if="burger.gluten == 'true'">
         Contains gluten
-        </span>
         <span v-if="burger.lactose == 'true'">
         Contains lactose
         </span>
      </div>
 </div> */
 
+/*
 var menu = [burger1, burger2, burger3, burger4, burger5];
 const vm = new Vue({
   el: '#burger',
@@ -19,3 +19,5 @@ const vm = new Vue({
     burgers: menu
   },
 })
+
+*/
